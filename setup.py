b7c43c5d92a5bd0704ea0a324b1619c9d1028a9b
Nico <nico.pro412@gmail.com>
2020-02-07 16:03:30 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-exec(open('influencer-centrality/version.py').read())
+exec(open('influencer/version.py').read())
 setup(
-   name='influencer-centrality',
+   name='influencer',
    version=__version__,
    description='A python library to find influencer in a social network',
    license="LICENSE.txt",
    long_description=open('README.md').read(),
    author='Nicola Procopio',
    url="https://github.com/nickprock/",
-   packages=['influencer-centrality'],  #same as name
-   install_requires=['numpy', 'jax', 'jaxlib'], #external packages as dependencies
+   packages=['influencer'],  #same as name
+   install_requires=['numpy'], #external packages as dependencies
 )
