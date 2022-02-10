989ac0452391ecffebb26ced2ec39c44f31a5c10
nickprock <nico.pro412@gmail.com>
2022-02-10 14:55:04 +0100

    updated init
    added torch_centrality

--- influencer/__init__.py
+++ influencer/__init__.py
@@ -1,4 +1,5 @@
 from .centrality import hits, tophits, socialAU
+from .torch_centrality import hits, tophits
 from .edge2adj import edge2adj
 # from .lazy_centrality import hits, tophits, socialAU
 from .version import __version__
